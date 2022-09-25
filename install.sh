f9b9162f487dec39bbd2a20695cac67e0433bc9f
kevinriex <info@kevinriexinger.de>
2022-09-26 00:44:32 +0200

    .

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sudo apt update
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
 
-sudo curl -L "https://github.com/docker/compose/releases/download/2.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
