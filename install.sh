e8b3608e57701291aec77bb4c6c77a93a17830b8
kevinriex <info@kevinriexinger.de>
2022-09-26 00:29:49 +0200

    Adding

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt-get install neofetch
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install neofetch -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo systemctl status docker
 
 mkdir -p ~/.docker/cli-plugins/
