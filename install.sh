28f202a2e1ed486615166f8bba8833b65013ca96
kevinriex <info@kevinriexinger.de>
2022-09-26 00:24:56 +0200

    Adding Docker

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
 
+sudo apt update
+sudo apt upgrade
 sudo apt-get install neofetch
- 
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+
